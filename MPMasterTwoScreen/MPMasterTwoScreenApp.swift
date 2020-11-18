8068c4ed9976a178f4bd960e7a54bfa8e2b1922d
Steven Kirke <Sfx.montage@gmail.com>
2020-11-18 11:17:10 +0300

    Initial Commit

---
+++ MPMasterTwoScreen/MPMasterTwoScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPMasterTwoScreenApp.swift
+//  MPMasterTwoScreen
+//
+//  Created by JARVIS on 18.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MPMasterTwoScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
